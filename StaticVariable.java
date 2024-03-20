1404d9c336c2c3ac2c1746c780c481e478713f89
akankshavns <makanksha858@gmail.com>
2024-03-21 00:22:01 +0530

    introdusing static keywords

--- StaticVariable.java
+++ StaticVariable.java
@@ -1,6 +1,32 @@
+import java.util.*;
+class StaticDemo {
 
-
+    static String Name, Enroll; 
+    int  sem;
+    static void fun(){
+        System.out.println("your name is:- " + Name + "\nyour Enroll is :- " + Enroll  );
+        
+    }
+   
+    StaticDemo(){
+        sem = 0;
+        // System.out.print(sem);
+    }
+    
+}
 public class StaticVariable {
-    public static void main(String[] args);
+    // static String Enroll;
+    public static void main(String[] args){
+        System.out.println("Enter your name, Enrollment, semeter:- ");
+        Scanner sc = new Scanner(System.in);
+       StaticDemo.Name = sc.nextLine();
+       StaticDemo.Enroll=sc.nextLine();
+       int num= sc.nextInt();
+       StaticDemo.fun();
+       StaticDemo obj1 = new StaticDemo();
+       obj1.sem = num;
+       System.out.print(obj1.sem);
+
+    }
 
 }
